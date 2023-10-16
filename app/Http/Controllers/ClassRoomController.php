48993f67b4481077038ba0b7986e8b665ff9a6a0
unknown <tharushafdo2000@gmail.com>
2023-10-16 21:10:20 +0530

    Lecture And ClassRoom Model Create With -A

---
+++ app/Http/Controllers/ClassRoomController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClassRoom;
+use App\Http\Requests\StoreClassRoomRequest;
+use App\Http\Requests\UpdateClassRoomRequest;
+
+class ClassRoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreClassRoomRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ClassRoom $classRoom)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ClassRoom $classRoom)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateClassRoomRequest $request, ClassRoom $classRoom)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ClassRoom $classRoom)
+    {
+        //
+    }
+}
