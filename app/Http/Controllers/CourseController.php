4fd711e0f4c1135a87d8bda4cff7ea428c8d6425
unknown <tharushafdo2000@gmail.com>
2023-10-20 01:58:25 +0530

    Courses Module

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use App\Http\Requests\StoreCourseRequest;
 use App\Http\Requests\UpdateCourseRequest;
+use Illuminate\Http\Request;
+use Throwable;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Str;
+
 
 class CourseController extends Controller
 {
@@ -13,6 +18,7 @@ class CourseController extends Controller
      */
     public function index()
     {
+        return view('courses.index');
         //
     }
 
@@ -21,6 +27,8 @@ class CourseController extends Controller
      */
     public function create()
     {
+        return view('courses.create');
+
         //
     }
 
@@ -61,6 +69,59 @@ class CourseController extends Controller
      */
     public function destroy(Course $course)
     {
+        $course->delete();
+        return response()->json(['status' => 'success', 'message' => 'Course Deleted Successfully.'], 200);
         //
+    }
+
+
+
+    public function getData(Request $request)
+    {
+        try {
+            $courses = Course::query()
+                ->orderBy('created_at', 'desc');
+
+            return DataTables::of($courses)
+                ->editColumn('course_code', function ($course) {
+                    return $course->course_code ;
+                })->editColumn('course_name', function ($course) {
+                    return $course->course_name ;
+                })
+                ->addColumn('description', function ($course) {
+                    // Str::limit($course->description, $limit = 20, $end = '...')
+                    // return $course->description ;
+                    return Str::limit($course->description, $limit = 20, $end = '...') ;
+                })
+                // ->addColumn('address', function ($course) {
+                //     return $course->Student ? $course->Student->address : "";
+                // })
+                // ->addColumn('tp', function ($course) {
+                //     return $course->Student ? $course->Student->tp : "";
+                // })
+                ->addColumn('actions', function ($course) {
+                    $route = route('courses.edit', ['course' => $course]);
+                
+                    $htmlContent = '
+                        <a rel="tooltip" class="btn btn-success btn-link" href="' . $route . '" data-original-title="" title="">
+                            <i class="material-icons">edit</i>
+                            <div class="ripple-container"></div>
+                        </a>
+                        
+                        <button data-id="' . $course->id . '" class="btn btn-danger btn-link deleteBtn" data-original-title="" title="">
+                            <i class="material-icons">close</i>
+                            <div class="ripple-container"></div>
+                        </button>
+                        
+                    ';
+                
+                    return $htmlContent;
+                })
+                // ->rawColumns(['course_code', 'course_name','description','address','tp','actions'])
+                ->rawColumns(['course_code', 'course_name','description','actions'])
+                ->make(true);
+        } catch (Throwable $th) {
+            dd($th->getMessage());
+        }
     }
 }
